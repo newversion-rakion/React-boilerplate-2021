fcb63f303d9ba0527f7bc51ee666297a473a5784
Viet Anh Nguyen <nguyenvietanh.work@gmail.com>
2023-04-12 15:57:37 +0700

    Update 2023

---
+++ app/containers/App/index.js
@@ -0,0 +1,47 @@
+/**
+ *
+ * App
+ *
+ * This component is the skeleton around the actual pages, and should only
+ * contain code that should be seen on all pages. (e.g. navigation bar)
+ */
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { Switch, Route } from 'react-router-dom';
+
+import { useInjectSaga } from 'utils/injectSaga';
+import { useInjectReducer } from 'utils/injectReducer';
+import RouterInterceptor from 'utils/routerInterceptor';
+
+import AdminLayout from 'layouts/AdminLayout';
+import ScrolToTop from 'components/ScrollToTop';
+
+import { ROUTER_LOGIN } from 'utils/constants';
+
+import Login from 'containers/Login/Loadable';
+import NotFoundPage from 'containers/NotFoundPage/Loadable';
+
+import GlobalStyle from '../../global-styles';
+
+import reducer from './reducer';
+import saga from './saga';
+
+export default function App() {
+  useInjectReducer({ key: 'global', reducer });
+  useInjectSaga({ key: 'global', saga });
+  return (
+    <div>
+      <Helmet titleTemplate="Test" defaultTitle="Test">
+        <meta name="description" content="Test" />
+      </Helmet>
+      <ScrolToTop />
+      <Switch>
+        <Route path="/" exact component={Login} />
+        <Route path={ROUTER_LOGIN} exact component={Login} />
+        <RouterInterceptor path="/in" component={AdminLayout} />
+        <Route path="" component={NotFoundPage} />
+      </Switch>
+      <GlobalStyle />
+    </div>
+  );
+}
